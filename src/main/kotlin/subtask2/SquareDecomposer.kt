588e8d64a25fe9d1aa0c9341d9beed9dcb2fc3dd
m4sterX <3119009@gmail.com>
2021-07-14 23:31:49 +0300

    all is done

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -2,8 +2,23 @@ package subtask2
 
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        return decompose(number, sqrtWithLong(number.toLong()))
     }
+
+    private fun decompose(x: Int, sqrt: Long): Array<Int>? {
+        for (i in x - 1 downTo 1) {
+            if (sqrtWithLong(i.toLong()) == sqrt)
+                return arrayOf(i)
+            val difference = sqrt - sqrtWithLong(i.toLong())
+            if (difference > 0) {
+                val arr = decompose(i, difference)
+                if (arr != null)
+                    return arr.plus(i)
+            }
+        }
+        return null
+    }
+
+    private fun sqrtWithLong(i: Long) = (i * i)
 }
--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -1,9 +1,48 @@
-package subtask3
-
 class TelephoneFinder {
 
-    // TODO: Complete the following function
+
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+        val num = number.toIntOrNull()
+        if (num == null || num < 0) return null
+        val newList = mutableListOf<String>()
+        val numInt = number
+            .split("")
+            .toMutableList()
+
+        numInt.removeAll { it == "" }
+
+        numInt.forEachIndexed { index, it ->
+            changeAllNaiborChar(number, index, collection[it]!!, newList)
+        }
+        return newList.toTypedArray()
+    }
+
+    private fun changeAllNaiborChar(
+        number: String,
+        index: Int,
+        array: Array<String>,
+        newArray: MutableList<String>
+    ) {
+        for (element in array) {
+            newArray.add(changeChar(number, index, element))
+        }
+    }
+
+    private fun changeChar(number: String, index: Int, charToReplace: String) =
+        number.substring(0, index) + charToReplace + number.substring(index + 1)
+
+    companion object {
+        val collection = mapOf(
+            "1" to arrayOf("2", "4"),
+            "2" to arrayOf("1", "3", "5"),
+            "3" to arrayOf("2", "6"),
+            "4" to arrayOf("1", "5", "7"),
+            "5" to arrayOf("2", "4", "6", "8"),
+            "6" to arrayOf("3", "5", "9"),
+            "7" to arrayOf("4", "8"),
+            "8" to arrayOf("5", "7", "9", "0"),
+            "9" to arrayOf("6", "8"),
+            "0" to arrayOf("8")
+        )
     }
 }
